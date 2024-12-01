2c1a85d26d4dcf584e668ddb1e6154fe9ec68539
shodan <mega@ioexception.at>
2024-12-01 23:52:46 +0100

    More readable golf

--- 01/sollution-golf.sh
+++ 01/sollution-golf.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 # Make sure to pass the file as the first argument of this script
 
-i=$1;f(){ cut -d' ' -f$1<$i | sort -n;};echo $(($(for l in $(paste -d'-' <(f 1) <(f 4));do l=$((l));echo +${l/-/};done)))
+i=$1;f(){ cut -d' ' -f$1<$i | sort -n;}
+echo $(($(for l in $(paste -d'-' <(f 1) <(f 4));do l=$((l));echo +${l/-/};done)))
