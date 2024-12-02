86e1e476beb66b3f4748fee440a7a4b428c7582f
shodan <mega@ioexception.at>
2024-12-02 01:07:01 +0100

    Shorter

--- 01/sollution2-golf.sh
+++ 01/sollution2-golf.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # Make sure to pass the file as the first argument of this script
 
-echo $((0$(for l in $(cut -d\  -f1<$1);do echo +$(($(grep -c \ $l $1)*$l));done)))
+echo $(($(for l in $(cut -d\  -f1<$1);do echo +$(($(grep -c \ $l $1)*$l));done)))
