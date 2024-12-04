70e9eaa3d46206bfccfc0aacc2a147e39da40371
shodan <mega@ioexception.at>
2024-12-04 22:32:04 +0100

    shorter

--- 04/part1.js
+++ 04/part1.js
@@ -1,6 +1,6 @@
 i = require('fs').readFileSync('input.txt', 'utf8');
 
 console.log(
-    f=[1,0,-1],g=i.split("\n"),g.map((l,x)=>([...l].map((_,y)=>f.map(X=>f.map(Y=>[...'XMAS'].every((c,i)=>c==g[x+X*i]?.[y+Y*i])))))).flat(9).reduce((o,x)=>+x+o)
+    f=[1,0,-1],g=i.split`\n`,eval(g.map((l,x)=>([...l].map((_,y)=>f.map(X=>f.map(Y=>+[...'XMAS'].every((c,i)=>c==g[x+X*i]?.[y+Y*i])))))).flat(9).join`+`)
 );
 
