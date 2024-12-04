a6232f3256701218aa48eccac9aa27e4e3c02799
shodan <mega@ioexception.at>
2024-12-04 01:11:30 +0100

    Day2 cursed

--- 02/part1.c
+++ 02/part1.c
@@ -1,8 +1,15 @@
+#if false
+# Run this using `bash part1.c`
+o=part1
+gcc -D L="$(echo \{0,$(while read l; do echo ${l// /,},0,;done<input.txt)0}\;)" $o.c -o $o.out
+./$o.out
+exit
+#endif
 #include <stdio.h>
-#include "input.h"
 
+int LL[]=L;
 int main() {
-    int*l=L,v,o=0,s,d;
-    while(C){if(!l[1]){C--;l+=2;o+=v;v=1;s=(*l-l[1]);s=(s>0)-(s<0);}d=s*(*l-*++l);if(d<1||d>3)v=0;}
+    int *l=LL,v,o=0,s,d;
+    while(*l|l[1]){if(!l[1]){l+=2;o+=v;v=1;s=(*l-l[1]);s=(s>0)-(s<0);}d=s*(*l-*++l);if(d<1||d>3)v=0;}
     printf("%i",o);
 }
