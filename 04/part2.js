da425f7a3aec8d6697d9f050b409b4330aca9c7e
Bas Bieling <bas@scrumble.nl>
2024-12-05 09:33:12 +0100

    smol

--- 04/part2.js
+++ 04/part2.js
@@ -1,5 +1,5 @@
 i = require('fs').readFileSync('input.txt', 'utf8');
 
 console.log(
-    g=i.split`\n`,eval(g.flatMap((l,x)=>[...l].map((c,y)=>c=='A'&[1,-1].every(X=>/MS|SM/.test(g[x+X]?.[y+1]+g[x-X]?.[y-1])))).join`+`)
+    g=i.split`\n`,eval(g.flatMap((l,x)=>[...l].map((c,y)=>c<'B'&[1,-1].every(X=>/MS|SM/.test(g[x+X]?.[y+1]+g[x-X]?.[y-1])))).join`+`)
 )
