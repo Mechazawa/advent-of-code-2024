1da8a996abedfe18fb392812d3cbb6e1187bc26f
Bas Bieling <bas@scrumble.nl>
2024-12-06 10:27:30 +0100

    Changes from last night

--- 05/part1.js
+++ 05/part1.js
@@ -1,3 +1,3 @@
 i=require('fs').readFileSync(0,'utf8').split`\n\n`
 r=i[0].replaceAll(/(..)\|(..)/g,'$2.+$1').replace(/\n/g,'|')
-console.log(eval(i[1].split`\n`.filter(x=>!x.match(r)&&x).map(x=>x.split(',')[x.match(/,/g).length/2]).join`+`))
+console.log(eval(i[1].split`\n`.map(x=>!x.match(r)&&x.split`,`[x.match(/,/g)?.length/2]||0).join`+`))
---
+++ 05/part2.js
@@ -0,0 +1,9 @@
+const tap = (x, fn) => {
+    fn(x);
+    return x;
+}
+
+i=require('fs').readFileSync(0,'utf8').split`\n\n`
+r=i[0].replaceAll(/(..)\|(..)/g,'$2.+$1').replace(/\n/g,'|')
+P=A=>A.reduce((a, v, i)=>a.concat(P([...A.slice(0, i),...A.slice(i + 1)]).map(val =>[v,...val])),[]);
+console.log(eval(i[1].split`\n`.filter(x=>x.match(r)).map(x=>{z=x.split`,`;P(z).filter(p=>!p.join`,`.match(r))[0][z.length/2]||0}).join`+`))
