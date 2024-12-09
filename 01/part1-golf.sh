2ab32d2c6c07914176d3875bddeed3f79e7b43c5
Bas Bieling <bas@scrumble.nl>
2024-12-09 10:52:23 +0100

    golf

--- 01/part1-golf.sh
+++ 01/part1-golf.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
-# Make sure to pass the file as the first argument of this script
 
 cat>a
 f(){ cut -d\  -f$1<a|sort -n;}
-echo $(($(for l in $(paste -d- <(f 1) <(f 4));do l=$((l));echo +${l/-/};done)))
+echo $(($(for l in $(paste -d- <(f 1) <(f 4));do echo +$((l));done|tr -d -)))
