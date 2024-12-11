fb03526a321d5f4895d2e8acab8393f8a66a641a
Bas Bieling <bas@scrumble.nl>
2024-12-11 13:09:16 +0100

    lint

--- 07/part1.js
+++ 07/part1.js
@@ -1,13 +1,13 @@
-i=require('fs').readFileSync(0,'utf8').split`\n`
+i = require('fs').readFileSync(0, 'utf8')
 
-console.log(i.reduce((a,l,I) => {
+console.log(i.split`\n`.reduce((a, l, I) => {
     const [target, ...parts] = l.split(/:? /).map(Number)
 
-    process.stdout.write("[ ] " + (I+1) + '/' + i.length+'\t'+l)
+    process.stdout.write("[ ] " + (I + 1) + '/' + i.length + '\t' + l)
 
-    for(let i = 0; i < 2**parts.length; i++) {
+    for (let i = 0; i < 2 ** parts.length; i++) {
         for (let j = 0; j < i; j++) {
-            const sum = parts.reduce((a,p,k) => j&(1<<k) ? a+p:a*p);
+            const sum = parts.reduce((a, p, k) => j & (1 << k) ? a + p : a * p);
             if (sum === target) {
                 process.stdout.write("\r[√\n")
                 return a + sum;
@@ -16,4 +16,4 @@ console.log(i.reduce((a,l,I) => {
     }
     process.stdout.write("\r[X\n")
     return a;
-},0))
+}, 0))
