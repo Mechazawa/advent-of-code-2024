cf6883623f65772c039cad7b1ee6c65bc75ddc87
Bas Bieling <bas@scrumble.nl>
2024-12-11 13:06:54 +0100

    Day 7.1

---
+++ 07/part1.js
@@ -0,0 +1,19 @@
+i=require('fs').readFileSync(0,'utf8').split`\n`
+
+console.log(i.reduce((a,l,I) => {
+    const [target, ...parts] = l.split(/:? /).map(Number)
+
+    process.stdout.write("[ ] " + (I+1) + '/' + i.length+'\t'+l)
+
+    for(let i = 0; i < 2**parts.length; i++) {
+        for (let j = 0; j < i; j++) {
+            const sum = parts.reduce((a,p,k) => j&(1<<k) ? a+p:a*p);
+            if (sum === target) {
+                process.stdout.write("\r[√\n")
+                return a + sum;
+            }
+        }
+    }
+    process.stdout.write("\r[X\n")
+    return a;
+},0))
