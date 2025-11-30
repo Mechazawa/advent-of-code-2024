5e03b0e4b1de9461a14cbd8a7d669040287c8208
Bas Bieling <bas@scrumble.nl>
2025-11-30 19:11:37 +0100

    20.1

--- 20/part1.js
+++ 20/part1.js
@@ -1,8 +1,8 @@
-i=require('fs').readFileSync(0,'utf8');
+i=require('fs').readFileSync(0,'utf8')
 
-I=i.split`\n`;X=I[0].length;Y=I.length;M=[];H='#';E='E';A=Math.abs
-I.map(((a,y)=>a.split``.map((_,x)=>M[+y*X+x]=a[x]==H?H:a[x]=='S'?0:null)))
+I=i.split`\n`;X=I[0].length;Y=I.length;M=[];H='#';A=Math.abs
+I.map((a,y)=>a.split``.map((_,x)=>M[y*X+x]=a[x]==H?H:a[x]=='S'?0:null))
 for(v=0;v<X*Y;v++)for(V=0;V<X*Y;V++)if(M[V]==v)[X,-X,-1,1].map(x=>M[x+V]??=v+1)
-c=0;for(i=0;i<X*Y;i++)for(x=-2;(d=M[i])!=H&&x<3;x++)for(y=-2;y<3;y++)c+=(m=A(x)+A(y))<3&&M[(Math.floor(i/X)+y)*X+i%X+x]-d-m>99
+c=0;for(i=0;i<X*Y;i++)for(x=-2;(d=M[i])!=H&&x<3;x++)for(y=-2;y<3;y++)c+=(m=A(x)+A(y))<3&&M[((i/X|0)+y)*X+i%X+x]-d-m>99
 
-console.log(c);
+console.log(c)
