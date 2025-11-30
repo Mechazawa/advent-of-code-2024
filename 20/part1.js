fccb98f451a04061e3090736b7760c8ddbadeaf4
Bas Bieling <bas@scrumble.nl>
2025-11-30 18:56:17 +0100

    20.1

--- 20/part1.js
+++ 20/part1.js
@@ -1,34 +1,8 @@
-i = require('fs').readFileSync(0, 'utf8');
+i=require('fs').readFileSync(0,'utf8');
 
-I = i.split`\n`
-X = I[0].length
-Y = I.length
-M = []
-H = '#'
-E = 'E'
-T = 101 // sub1
-// Make map
-I.map(((a, y) => a.split``.map((_, x) => M[+y * X + x] = a[x] == H ? H : a[x] == 'S' ? 0 : null)))
-// floodfill
-for (v = 0; v < X * Y; v++) for (V = 0; V < X * Y; V++) if (M[V] == v) [X, -X, -1, 1].map(x => M[x + V] ??= v + 1);
-// find shortcuts
-console.log(
-    [...M].filter((d, i) =>
-        d != H &&
-        (i % X < X - 1) &&
-        i + X < X * Y &&
-        // [X * 2, 2, X + 1].flatMap(o => [X, -X, 1, -1].map(O => Math.abs(M[o + O + i] - d))).some(t=>t < T)
-        [i%X<X-1&&X*2+1,X*2-1,X*3,i%X<X-2&&X+2,2-X,i%X<X-3&&3].filter(x=>x&&M[x+i]>=0).map(O => Math.abs(M[O + i] - d)+2).some(t=>t < T)
-    ).length
-);
+I=i.split`\n`;X=I[0].length;Y=I.length;M=[];H='#';E='E';A=Math.abs
+I.map(((a,y)=>a.split``.map((_,x)=>M[+y*X+x]=a[x]==H?H:a[x]=='S'?0:null)))
+for(v=0;v<X*Y;v++)for(V=0;V<X*Y;V++)if(M[V]==v)[X,-X,-1,1].map(x=>M[x+V]??=v+1)
+c=0;for(i=0;i<X*Y;i++)for(x=-2;(d=M[i])!=H&&x<3;x++)for(y=-2;y<3;y++)c+=(m=A(x)+A(y))<3&&M[(Math.floor(i/X)+y)*X+i%X+x]-d-m>99
 
-// debug
-function p() {
-    o = ''
-    for (i = 0; i < X * Y; i++) {
-        if (i % X == 0) o += '\n|';
-        if (M[i] == H) o += '####|';
-        else o += ("" + (M[i] ?? ' ')).padStart(4) + '|'
-    }
-    console.log(o);
-}
+console.log(c);
